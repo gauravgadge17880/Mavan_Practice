78ac7ae557dfc2598ef5644c026f79cb93c10b1e
gaurav <gauravgadge17880@gmail.com>
2024-07-05 17:06:00 +0530

    this is practice mavan project

---
+++ Maven_Practice/src/test/java/com/example/demo/MavenPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
